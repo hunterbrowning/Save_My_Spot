cc6483687a6539b98c6f7dc4b5606643ee849d6b
Hunter Browning <hpbrowning93@gmail.com>
2014-05-13 22:45:59 -0500

    * BullShit.cs: Added "BullShit.cs" for adam
    * Save_My_Spot.userprefs:
    * BullShit.xib:
    * Save_My_Spot.csproj:
    * BullShit.designer.cs:

---
+++ Save_My_Spot/BullShit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace Save_My_Spot
+{
+	public partial class BullShit : UIViewController
+	{
+		// this class is to test how xamarin handles changes to an .xib file made through source code and opened without xamarin.
+		public BullShit () : base ("BullShit", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+	}
+}
+
